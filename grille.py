94212c798984ad6ec156718c50f42527a102e461
Martin Bozon <62135515+martin-bozon@users.noreply.github.com>
2021-11-23 14:38:09 +0100

    Fait - plateau de jeu

--- grille.py
+++ grille.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 import pygame
-import MUR from config
-import CAISSE from config
-import OBJECTIF from config
+from config import *
 
 class Grille:
-    def __init__(self):
+    def __init__(self, fichier):
         self.ref_img = {
             MUR: pygame.image.load("img/mur.png"),
             CAISSE: pygame.image.load("img/banane.png"),
             OBJECTIF: pygame.image.load("img/objectif.png"),
             #CAISSE_OK: pygame.image.load("img/caisse_ok.jpg"),
         }
-        with open ('matrice.txt', 'r') as fich:
-            self.lvl = [[int(l) for l in line.strip().split(" ")] for line in fich]
+        with open(fichier, 'r') as fich:
+            self.lvtest = [[int(l) for l in line.strip().split(" ")] for line in fich]
 
         self.coord_objec = []
         for y in range(len(self.lvtest)):
@@ -21,7 +19,7 @@ class Grille:
                 if self.lvtest[y][x] == OBJECTIF:
                     self.coord_objec.append((x, y))
 
-    def drawMap:
+    def drawMap(self, screen):
         for y in range(len(self.lvtest)):
             for x in range(len(self.lvtest[y])):
                 img = self.lvtest[y][x]
@@ -29,3 +27,12 @@ class Grille:
                     x += 1
                 else:
                     screen.blit(self.ref_img[img], (x * SIZE, y * SIZE))
+
+    def is_fini(self):
+        lis = [self.lvtest[y][x] for (x, y) in self.coord_objec]
+        return lis.count(CAISSE_OBJECTIF) == len(self.coord_objec)
+
+
+if __name__ == '__main__':
+    g = Grille()
+    g.genMap("matrice.txt")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
-import LARGEUR from config
-import HAUTEUR from config
-import TITRE from config
+import sys
+from pygame.locals import *
+from config import *
+from grille import *
 #initalise les modules pour pygame
 pygame.init()
 
@@ -15,3 +16,21 @@ _grille = Grille("matrice.txt")
 _grille.drawMap(screen)
 
 pygame.display.flip()
+
+continuer = True
+while not _grille.is_fini():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == KEYDOWN:
+            # _player.move(event.key)
+            print('toto')
+            if event.key == K_r:
+                _grille.genMap("lvl/lv1")
+                _grille.drawMap(screen)
+                # _player = Player(_grille)
+                # _player.drawPlayer(screen)
+    screen.blit(background, (0,0))
+    _grille.drawMap(screen)
+    # _player.drawPlayer(screen)
+    pygame.display.flip()
